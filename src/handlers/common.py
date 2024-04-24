2cc01ba749253a3814d3e43b411a21281474d840
Richtermnd <m.rustam1305@gmail.com>
2024-04-24 19:27:56 +0500

    Change some messages.

--- src/handlers/common.py
+++ src/handlers/common.py
@@ -3,13 +3,13 @@ from aiogram import filters, types
 from aiogram.fsm import context, state
 
 from keyboards import keyboard
+from handlers import enums
 
 router = aiogram.Router()
 
 
 @router.message(filters.Command(commands=["start"]))
 async def cmd_start(message: types.Message, state: context.FSMContext):
-    # await message.answer("Информация о боте, политика конфиденциальности и т.д.\n/help чтобы узнать больше")
     await message.answer(
-        "Этот бот создан для сбора информации о пострадавших в Оренбурге во время затопления.",
+        enums.FormTexts.start_message,
         reply_markup=keyboard.make_keyboard([["Заполнить форму"]]))
